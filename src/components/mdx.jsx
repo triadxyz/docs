b7d6863a39c25399061892d8086dd5ab9beee169
dannpl <dancardosobr@gmail.com>
2024-10-06 02:02:15 +0700

    pickax

--- src/components/mdx.jsx
+++ src/components/mdx.jsx
@@ -7,6 +7,10 @@ export const a = Link
 export { Button } from '../components/Button'
 export { CodeGroup, Code as code, Pre as pre } from '../components/Code'
 
+export const h1 = function H1(props) {
+  return <Heading className="mb-6" level={1} {...props} />
+}
+
 export const h2 = function H2(props) {
   return <Heading level={2} {...props} />
 }
